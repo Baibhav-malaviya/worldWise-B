7b10d333fc376e22ba0ff3e742bb6744187abfc5
Baibhav Kumar <baibhav.kr73@gmail.com>
2023-11-19 11:33:00 +0530

    bcrypt is add to package.json, and apply the password hashing

--- src/model/User.model.js
+++ src/model/User.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
 	{
@@ -25,6 +26,17 @@ const userSchema = new Schema(
 	},
 	{ timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) return next();
+
+	try {
+		this.password = await bcrypt.hash(this.password, 10);
+		next();
+	} catch (error) {
+		next(error);
+	}
+});
 
 const User = model("User", userSchema);
 
